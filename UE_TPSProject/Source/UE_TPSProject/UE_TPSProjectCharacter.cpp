d4ef1480543ffd5090375231cca29643b4de957f
christian buffa <christian.b2402@gmail.com>
2023-10-27 17:34:16 +0200

    fixed character

--- UE_TPSProject/Source/UE_TPSProject/UE_TPSProjectCharacter.cpp
+++ UE_TPSProject/Source/UE_TPSProject/UE_TPSProjectCharacter.cpp
@@ -1,26 +1,25 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UE_TPSProjectCharacter.h"
-#include "Engine/LocalPlayer.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/Controller.h"
-#include "EnhancedInputComponent.h"
-#include "EnhancedInputSubsystems.h"
-#include "InputActionValue.h"
-
-DEFINE_LOG_CATEGORY(LogTemplateCharacter);
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 //////////////////////////////////////////////////////////////////////////
-// AUE_TPSProjectCharacter
+// ATP_ThirdPersonCharacter
 
-AUE_TPSProjectCharacter::AUE_TPSProjectCharacter()
-{
+AUE_TPSProjectCharacter::AUE_TPSProjectCharacter() {
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-		
+	
+	// set our turn rates for input
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
@@ -28,103 +27,381 @@ AUE_TPSProjectCharacter::AUE_TPSProjectCharacter()
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
-
-	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
-	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
-	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
-	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
-	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
-	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+	GetCharacterMovement()->GetNavAgentPropertiesRef().bCanCrouch = true;
+	GetCharacterMovement()->SetCrouchedHalfHeight(52.0f);
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-
+	
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+		
+	// Add a mesh for the weapon
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, "hand_rSocket");
+	
+	//Set other variabled
+	ActiveWeapon = 0;
+	ActiveThrowable = 0;
+	CheckCoverRadius = 60.0f;
+	ActualEight = GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+	bIsAiming = false;
 }
 
-void AUE_TPSProjectCharacter::BeginPlay()
-{
-	// Call the base class  
+void AUE_TPSProjectCharacter::BeginPlay() {
 	Super::BeginPlay();
+	
+	bCanMove = true;
+	MagBullets = Arsenal[ActiveWeapon].MagCapacity;
+	FireTime = 0.0f;
+	FVector WeaponLocation = GetMesh()->GetSocketLocation("hand_rSocket");
+	FRotator WeaponRotaion = GetMesh()->GetSocketRotation("hand_rSocket");
+	
+	MaxSpeedWalkingOrig = GetCharacterMovement()->MaxWalkSpeed;
+	if (MovementCurve && OffsetCurve) {
+		FOnTimelineFloat ProgressFunctionLength;
+		ProgressFunctionLength.BindUFunction(this, "HandleProgressArmLength");
+		AimTimeline.AddInterpFloat(MovementCurve, ProgressFunctionLength);
 
-	//Add Input Mapping Context
-	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
-	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-		{
-			Subsystem->AddMappingContext(DefaultMappingContext, 0);
-		}
+		FOnTimelineVector ProgressFunctionOffset;
+		ProgressFunctionOffset.BindUFunction(this, "HandleProgressCameraOffset");
+		AimTimeline.AddInterpVector(OffsetCurve, ProgressFunctionOffset);
 	}
+
+	if (CrouchCurve) {
+		FOnTimelineFloat ProgressFunctionCrouch;
+		ProgressFunctionCrouch.BindUFunction(this, "HandleProgressCrouch");
+		CrouchTimeline.AddInterpFloat(CrouchCurve, ProgressFunctionCrouch);
+	}
+}
+
+void AUE_TPSProjectCharacter::OnConstruction(const FTransform & Transform) {
+	if (Arsenal.Num() > 0) {
+		WeaponMesh->SetStaticMesh(Arsenal[0].WeaponMesh);
+	}
+}
+
+
+void AUE_TPSProjectCharacter::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+	AimTimeline.TickTimeline(DeltaTime);
+	CrouchTimeline.TickTimeline(DeltaTime);
+	
+	AutomaticFire(DeltaTime);
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Timeline management
+
+void AUE_TPSProjectCharacter::HandleProgressArmLength(float Length) {
+	CameraBoom->TargetArmLength = Length;
+}
+
+void AUE_TPSProjectCharacter::HandleProgressCameraOffset(FVector Offset) {
+	CameraBoom->SocketOffset = Offset;
+}
+
+void AUE_TPSProjectCharacter::HandleProgressCrouch(float Height) {
+	ActualEight = Height;
 }
 
 //////////////////////////////////////////////////////////////////////////
 // Input
 
-void AUE_TPSProjectCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	// Set up action bindings
-	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
-		
-		// Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+void AUE_TPSProjectCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) {
+	// Set up gameplay key bindings
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AUE_TPSProjectCharacter::JumpCharacter);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AUE_TPSProjectCharacter::StopJumpingCharacter);
 
-		// Moving
-		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AUE_TPSProjectCharacter::Move);
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &AUE_TPSProjectCharacter::CrouchCharacter);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &AUE_TPSProjectCharacter::StopCrouchCharacter);
+	
+	PlayerInputComponent->BindAction("Aim", IE_Pressed, this, &AUE_TPSProjectCharacter::AimInWeapon);
+	PlayerInputComponent->BindAction("Aim", IE_Released, this, &AUE_TPSProjectCharacter::AimOutWeapon);
 
-		// Looking
-		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AUE_TPSProjectCharacter::Look);
-	}
-	else
-	{
-		UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
-	}
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AUE_TPSProjectCharacter::Fire);
+	PlayerInputComponent->BindAction("Fire", IE_Released, this, &AUE_TPSProjectCharacter::StopFire);
+	
+	PlayerInputComponent->BindAction("Reload", IE_Pressed, this, &AUE_TPSProjectCharacter::ReloadWeapon);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AUE_TPSProjectCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AUE_TPSProjectCharacter::MoveRight);
+
+	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+	// "turn" handles devices that provide an absolute delta, such as a mouse.
+	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AUE_TPSProjectCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AUE_TPSProjectCharacter::LookUpAtRate);
 }
 
-void AUE_TPSProjectCharacter::Move(const FInputActionValue& Value)
-{
-	// input is a Vector2D
-	FVector2D MovementVector = Value.Get<FVector2D>();
+//////////////////////////////////////////////////////////////////////////
+// Movement and rotation
 
-	if (Controller != nullptr)
-	{
+void AUE_TPSProjectCharacter::TurnAtRate(float Rate) {
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AUE_TPSProjectCharacter::LookUpAtRate(float Rate) {
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+
+void AUE_TPSProjectCharacter::MoveForward(float Value) {
+	if ((Controller != NULL) && (Value != 0.0f) & !bIsInCover && bCanMove) {
 		// find out which way is forward
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
 		// get forward vector
-		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	
-		// get right vector 
-		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-
-		// add movement 
-		AddMovementInput(ForwardDirection, MovementVector.Y);
-		AddMovementInput(RightDirection, MovementVector.X);
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
 	}
 }
 
-void AUE_TPSProjectCharacter::Look(const FInputActionValue& Value)
-{
-	// input is a Vector2D
-	FVector2D LookAxisVector = Value.Get<FVector2D>();
+void AUE_TPSProjectCharacter::MoveRight(float Value) {
+	if ((Controller != NULL) && (Value != 0.0f) && bCanMove) {
+		if (!bIsInCover) {
+			// find out which way is right
+			const FRotator Rotation = Controller->GetControlRotation();
+			const FRotator YawRotation(0, Rotation.Yaw, 0);
 
-	if (Controller != nullptr)
-	{
-		// add yaw and pitch input to controller
-		AddControllerYawInput(LookAxisVector.X);
-		AddControllerPitchInput(LookAxisVector.Y);
+			// get right vector 
+			const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+			// add movement in that direction
+			AddMovementInput(Direction, Value);
+		} 
 	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Mechanic: Aim
+
+void AUE_TPSProjectCharacter::AimInWeapon() {
+	if (!bIsUsingArch) {
+		bIsUsingWeapon = true;
+		AimIn();
+	}
+}
+
+void AUE_TPSProjectCharacter::AimOutWeapon() {
+	bIsUsingWeapon = false;
+	AimOut();
+}
+
+void AUE_TPSProjectCharacter::AimIn() {
+	GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("Aim In"));
+	bUseControllerRotationYaw = true;
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+	GetCharacterMovement()->MaxWalkSpeed = MaxSpeedAiming;
+	bIsAiming = true;
+	if (bIsInCover) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("Aim from cover"));
+		StopCrouchCharacter();
+	}
+	AimTimeline.Play();
+	OnCharacterAim.Broadcast();
+}
+
+void AUE_TPSProjectCharacter::AimOut() {
+	GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("Aim Out"));
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->MaxWalkSpeed = MaxSpeedWalkingOrig;
+	bIsAiming = false;
+	if (bIsInCover) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("Stop aim from cover"));
+		CrouchCharacter();
+	}
+	AimTimeline.Reverse();
+	OnCharacterStopAim.Broadcast();
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Mechanic: Jump
+
+void AUE_TPSProjectCharacter::JumpCharacter() {
+	if (CanJump()) {
+		Jump();
+		GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("jumping"));
+	}
+}
+
+void AUE_TPSProjectCharacter::StopJumpingCharacter() {
+	StopJumping();
+}
+
+void AUE_TPSProjectCharacter::Landed(const FHitResult& Hit) {
+	GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("landed"));
+	OnCharacterLanding.Broadcast();
+}
+
+/** This is a UE4 function of AActor class*/
+void AUE_TPSProjectCharacter::OnJumped_Implementation() { 
+	GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("On jumped"));
+	OnCharacterJumping.Broadcast();
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Mechanic: Crouch
+
+void AUE_TPSProjectCharacter::CrouchCharacter() {
+	
+	if (CanCrouch()) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("Crouch in"));
+		Crouch();
+		CrouchTimeline.Play();
+		OnCharacterCrouch.Broadcast();
+	}
+}
+
+void AUE_TPSProjectCharacter::StopCrouchCharacter() {
+	GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Green, TEXT("Uncrouch"));
+	if (GetCharacterMovement()->IsCrouching()) {
+		UnCrouch();
+		CrouchTimeline.Reverse();
+		OnCharacterUncrouch.Broadcast();
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Mechanic: Fire with weapon
+
+void AUE_TPSProjectCharacter::FireFromWeapon() {
+	if(bIsReloading){
+		return;
+	}
+	
+	FCollisionQueryParams Params;
+	// Ignore the player's pawn
+	Params.AddIgnoredActor(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));
+
+	// The hit result gets populated by the line trace
+	FHitResult Hit;
+
+	float WeaponRange = Arsenal[ActiveWeapon].Range;
+
+	FVector Start = FollowCamera->GetComponentLocation();
+	FVector End = Start + (FollowCamera->GetComponentRotation().Vector() * WeaponRange);
+
+	if (!bIsAiming) {
+		float WeaponOffset = Arsenal[ActiveWeapon].Offset;
+		GEngine->AddOnScreenDebugMessage(-1, 5.2f, FColor::Emerald, TEXT("Not aiming!"));
+		Start = WeaponMesh->GetComponentLocation() + (WeaponMesh->GetForwardVector() * WeaponOffset);
+		End = Start + (WeaponMesh->GetComponentRotation().Vector() * WeaponRange);
+	}
+
+	bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Pawn, Params);
+	OnCharacterTraceLine.Broadcast();
+
+	if (bHit) {
+		DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 3.0f);
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), Arsenal[ActiveWeapon].HitEFX, Hit.ImpactPoint);
+	}
+
+	UGameplayStatics::PlaySound2D(this, Arsenal[ActiveWeapon].SoundEFX, 1.0f, 1.0f, 0);
+}
+
+void AUE_TPSProjectCharacter::AutomaticFire(float DeltaTime) {
+	if (Arsenal[ActiveWeapon].IsAutomatic && bIsFiring) {
+		FireTime += DeltaTime;
+		if (FireTime >= Arsenal[ActiveWeapon].Rate) {
+			FireTime = 0.0f;
+			if (MagBullets > 0) {
+				FireFromWeapon();
+				MagBullets--;
+			} else {
+				StopFire();
+				ReloadWeapon();
+			}
+		}
+	}
+}
+
+void AUE_TPSProjectCharacter::Fire() {
+	if (!bIsReloading) {
+		bIsFiring = true;
+		FireTime = 0.0f;
+		if (MagBullets > 0) {
+			FireFromWeapon();
+			MagBullets--;
+		} else {
+			StopFire();
+			ReloadWeapon();
+		}
+	}
+}
+
+
+void AUE_TPSProjectCharacter::StopFire() {
+	bIsFiring = false;
+	FireTime = 0.0f;
+}
+
+FWeaponSlot AUE_TPSProjectCharacter::RetrieveActiveWeapon() {
+	return Arsenal[ActiveWeapon];
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// Mechanic: Reload
+
+void AUE_TPSProjectCharacter::ReloadWeapon() {
+	if(bIsReloading || MagBullets >= Arsenal[ActiveWeapon].MagCapacity){
+		return;
+	}
+	
+	if (!bIsUsingArch) {
+		GEngine->AddOnScreenDebugMessage(-1, 5.2f, FColor::Red, TEXT("Start Reload!"));
+		bIsReloading = true;
+		OnCharacterStartReload.Broadcast();
+	}
+}
+
+void AUE_TPSProjectCharacter::EndReload() {
+	GEngine->AddOnScreenDebugMessage(-1, 5.2f, FColor::Orange, TEXT("End Reload!"));
+	bIsReloading = false;
+	MagBullets = Arsenal[ActiveWeapon].MagCapacity;
+}
+
+int AUE_TPSProjectCharacter::MagCounter() {
+	return MagBullets;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Utilities
+
+void AUE_TPSProjectCharacter::EnablePlayerInput(bool Enabled) {
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+
+	if (Enabled) {
+		EnableInput(PlayerController);
+	} else {
+		DisableInput(PlayerController);
+	}
+}
+
+void AUE_TPSProjectCharacter::EnableMovement(bool Enabled) {
+	bCanMove = Enabled;
+}
+
+bool AUE_TPSProjectCharacter::IsAimingWithWeapon() {
+	return bIsAiming && bIsUsingWeapon;
+}
+
+bool AUE_TPSProjectCharacter::IsAimingWithArch() {
+	return bIsAiming && bIsUsingArch;
 }
--- UE_TPSProject/Source/UE_TPSProject/UE_TPSProjectCharacter.h
+++ UE_TPSProject/Source/UE_TPSProject/UE_TPSProjectCharacter.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/TimelineComponent.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
 #include "UE_TPSProjectCharacter.generated.h"
@@ -13,13 +14,52 @@ class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
 
+USTRUCT(BlueprintType)
+struct FWeaponSlot
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMesh *WeaponMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int MagCapacity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsAutomatic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Rate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Range;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float HitRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Offset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UParticleSystem* HitEFX;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundBase* SoundEFX;
+	
+	//FWeaponSlot();
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FGameStateCharacter);
 DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
 
 UCLASS(config=Game)
 class AUE_TPSProjectCharacter : public ACharacter
 {
 	GENERATED_BODY()
-
+	
 	/** Camera boom positioning the camera behind the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
@@ -27,44 +67,206 @@ class AUE_TPSProjectCharacter : public ACharacter
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
-	
-	/** MappingContext */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputMappingContext* DefaultMappingContext;
 
-	/** Jump Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* JumpAction;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UChildActorComponent* WeaponComponent;
 
-	/** Move Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* MoveAction;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true") )
+	UStaticMeshComponent* WeaponMesh;
 
-	/** Look Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* LookAction;
 
 public:
 	AUE_TPSProjectCharacter();
+
+	/** Broadcasted when character land on ground */
+	UPROPERTY(BlueprintAssignable)
+	FGameStateCharacter OnCharacterLanding;
+
+	/** Broadcasted when character jump */
+	UPROPERTY(BlueprintAssignable)
+	FGameStateCharacter OnCharacterJumping;
 	
+	/** Broadcasted when character crouching */
+	UPROPERTY(BlueprintAssignable)
+	FGameStateCharacter OnCharacterCrouch;
+	
+	/** Broadcasted when character stop crouching */
+	UPROPERTY(BlueprintAssignable)
+	FGameStateCharacter OnCharacterUncrouch;
+
+	/** Broadcasted when character start aiming */
+	UPROPERTY(BlueprintAssignable)
+	FGameStateCharacter OnCharacterAim;
+
+	/** Broadcsted when character stop aiming */
+	UPROPERTY(BlueprintAssignable)
+	FGameStateCharacter OnCharacterStopAim;
+
+	/** Broadcsted when character reloading the weapon */
+	UPROPERTY(BlueprintAssignable)
+	FGameStateCharacter OnCharacterStartReload;
+
+	/** Broadcsted when character trace line from weapon */
+	UPROPERTY(BlueprintAssignable)
+	FGameStateCharacter OnCharacterTraceLine;
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseLookUpRate;
+	
+	/** Walk speed while aiming. */
+	UPROPERTY(EditAnywhere, Category = "Aim")
+	float MaxSpeedAiming = 150.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aim")
+	float ActualEight = 0.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	UCurveFloat* MovementCurve;
+
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	UCurveVector* OffsetCurve;
+
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	UCurveFloat* CrouchCurve;
+	
+	/** This array contains all the character weapons*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapons")
+	TArray<FWeaponSlot> Arsenal;
+
+private:
+	int ActiveWeapon;
+
+	int ActiveThrowable;
+
+	int MagBullets;
+
+	float MaxSpeedWalkingOrig;
+
+	float FireTime;
+
+	float CheckCoverRadius;
+
+	bool bIsAiming;
+	
+	bool bCanTakeCover = false;
+	
+	bool bIsInCover = false;
+
+	bool bIsFiring = false;
+
+	bool bIsReloading = false;
+
+	bool bIsUsingArch = false;
+
+	bool bIsUsingWeapon = false;
+
+	bool bCanMove = false;
+	
+	/** Timeline use for aiming: change the visual from 360 to right shoulder*/
+	FTimeline AimTimeline;
+
+	/** Timeline used to crouch character*/
+	FTimeline CrouchTimeline;
+
+	/** This variable stores the allowed movement while player is covering*/
+	FVector CoverDirectionMovement;
+
+	UFUNCTION()
+	void HandleProgressArmLength(float Length);
+	
+	UFUNCTION()
+	void HandleProgressCameraOffset(FVector Offset);
+
+	UFUNCTION()
+	void HandleProgressCrouch(float Height);
+	
+	void EnablePlayerInput(bool Enabled);
+
+	void EnableMovement(bool Enabled);
+
+	// Mechanic: Fire with weapon
+	void FireFromWeapon();
+	void AutomaticFire(float DeltaTime);
+
 
 protected:
+	virtual void OnConstruction(const FTransform& Transform) override;
 
-	/** Called for movement input */
-	void Move(const FInputActionValue& Value);
+	// Mechanic: Movement and rotation
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
 
-	/** Called for looking input */
-	void Look(const FInputActionValue& Value);
-			
+	/** Called for side to side input */
+	void MoveRight(float Value);
 
-protected:
+	/** 
+	 * Called via input to turn at a given rate. 
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to turn look up/down at a given rate. 
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void LookUpAtRate(float Rate);
+	
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// End of APawn interface
 	
-	// To add mapping context
-	virtual void BeginPlay();
+	// Mechanic: Jump
+	void JumpCharacter();
+	void StopJumpingCharacter();
+
+	// Mechanic: Crouch
+	void CrouchCharacter();
+	void StopCrouchCharacter();
+
+	// Mechanic: Aim
+	void AimInWeapon();
+	void AimOutWeapon();
+	void AimIn();
+	void AimOut();
+
+	// Mechanic: Reload
+	void ReloadWeapon();
+
+	// 
 
 public:
+	
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void Landed(const FHitResult& Hit) override;
+	virtual void OnJumped_Implementation();
+
+	/** Called when the player try to fire with weapon */
+	void Fire();
+	/** Called when the player stop firing with weapon */
+	void StopFire();
+
+	UFUNCTION(BlueprintCallable, Category = "Reload")
+	void EndReload();
+
+	UFUNCTION(BlueprintCallable, Category = "TPS")
+	bool IsAimingWithWeapon();
+
+	UFUNCTION(BlueprintCallable, Category = "TPS")
+	bool IsAimingWithArch();
+
+	UFUNCTION(BlueprintCallable, Category = "Reload")
+	int MagCounter();
+
+	UFUNCTION(BlueprintCallable, Category = "TPS")
+	FWeaponSlot RetrieveActiveWeapon();
+
 	/** Returns CameraBoom subobject **/
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
